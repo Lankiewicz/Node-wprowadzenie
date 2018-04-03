f49aff1c784e6e2e1c18f3cec6b3a4be950b12dc
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-04-03 18:32:39 +0200

    update

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function getOSinfo() {
     var release = os.release();
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
-    var userInfo = os.userInfo();
+	var hours =  Math.floor(uptime / 3600);
+	var minutes =  Math.floor((uptime - hours * 3600) / 60);
+	var seconds = Math.floor (uptime - hours * 3600 - minutes * 60);
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 3600).toFixed(2), 'h');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+	console.log('Uptime: ~', hours, 'h', minutes, 'm', seconds, 's');
 }
 
 getOSinfo();
